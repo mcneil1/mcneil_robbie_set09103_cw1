bc5f52ac547b9f079e5ec1095f05ad13238e048e
Robbie McNeil <robbiemcn98@gmail.com>
2018-10-15 03:25:37 +0100

    added login page

--- 40296075cw.py
+++ 40296075cw.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def root():
-	return "Home page"
+def home():
+	return render_template('header.html')
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+	error= None
+	if request.method == 'POST':
+		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+			error = 'Invalid Credentials. Please try again.'
+		else:	
+			return redirect(url_for('home'))
+	return render_template('login.html', error=error, user=user)
+
+
 
 @app.route("/genre/")
 def genre():
