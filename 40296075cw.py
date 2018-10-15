5355bf5c3ea958d75c46b7f07c15e0460ec6a4c5
Robbie McNeil <robbiemcn98@gmail.com>
2018-10-15 02:01:30 +0100

    Changed route URL names, added new URLs for existing routes, adjusted /album route

--- 40296075cw.py
+++ 40296075cw.py
@@ -28,54 +28,59 @@ def albums():
 	
 	
 
-@app.route("/hip_hop/")
+@app.route("/genre/hip_hop/")
 def artist():
 	ImageNo='50%'
 	artistA='BROCKHAMPTON'
-	artistAurl='http://set09103.napier.ac.uk:9159/hip_hop/brockhampton/'
-	artistAimg=url_for('static',filename='brokhampton.jpg')
+	artistAurl='http://set09103.napier.ac.uk:9159/genre/hip_hop/brockhampton/'
+	artistAimg=url_for('static',filename='brockhampton.jpg')
 	artistB='Kids See Ghosts'
-	artistBurl='http://set09103.napier.ac.uk:9159/hip_hop/kids_see_ghosts/'
+	artistBurl='http://set09103.napier.ac.uk:9159/genre/hip_hop/kids_see_ghosts/'
 	artistBimg=url_for('static',filename='kidsSeeGhosts.jpg')
 	return render_template('genreArtist.html',ImageNo=ImageNo,artistA=artistA,artistAurl=artistAurl,artistAimg=artistAimg,artistB=artistB,artistBimg=artistBimg,artistBurl=artistBurl)
 
-@app.route("/house/")
+@app.route("/genre/house/")
 def album():
 	ImageNo='50%'
 	artistA="Daft Punk"
-	artistAurl="http://set09103.napier.ac.uk:9159/house/daft_punk/"
+	artistAurl="http://set09103.napier.ac.uk:9159/genre/house/daft_punk/"
 	artistAimg=url_for('static',filename='daftPunk.jpg')
 	artistB="Jamie xx"
-	artistBurl="http://set09103.napier.ac.uk:9159/house/jamie_xx/"
+	artistBurl="http://set09103.napier.ac.uk:9159/genre/house/jamie_xx/"
 	artistBimg=url_for('static',filename='jamiexx.jpg')
 	return render_template('genreArtist.html',ImageNo=ImageNo,artistA=artistA,artistAurl=artistAurl,artistAimg=artistAimg,artistB=artistB,artistBimg=artistBimg,artistBurl=artistBurl)
 
-@app.route("/RnB/")
+@app.route("/genre/RnB/")
 def rnb():
 	ImageNo='50%'
 	artistA="Childish Gambino"
-	artistAurl="http://set09103.napier.ac.uk:9159/RnB/childish_gambino/"
+	artistAurl="http://set09103.napier.ac.uk:9159/genre/RnB/childish_gambino/"
 	artistAimg=url_for('static',filename='childishGambino.jpg')
 	artistB="Frank Ocean"
-	artistBurl="http://set09103.napier.ac.uk:9159/RnB/frank_ocean/"
+	artistBurl="http://set09103.napier.ac.uk:9159/genre/RnB/frank_ocean/"
 	artistBimg=url_for('static',filename='frankOcean.jpg')
 	return render_template('genreArtist.html',ImageNo=ImageNo,artistA=artistA,artistAurl=artistAurl,artistAimg=artistAimg,artistB=artistB,artistBimg=artistBimg,artistBurl=artistBurl)
 
 	
 
-@app.route("/indie_rock/")
+@app.route("/genre/indie_rock/")
 def date():
 	ImageNo='50%'
 	artistA="The Strokes"
-	artistAurl="http://set09103.napier.ac.uk:9159/indie_rock/the_/strokes"
+	artistAurl="http://set09103.napier.ac.uk:9159/genre/indie_rock/the_/strokes"
 	artistAimg=url_for('static',filename='theStrokes.jpg')
 	artistB="The xx"
-	artistBurl="http://set09103.napier.ac.uk:9159/indie_rock/the_xx/"
+	artistBurl="http://set09103.napier.ac.uk:9159/genre/indie_rock/the_xx/"
 	artistBimg=url_for('static',filename='thexx.jpg')
 	return render_template('genreArtist.html',ImageNo=ImageNo,artistA=artistA,artistAurl=artistAurl,artistAimg=artistAimg,artistB=artistB,artistBimg=artistBimg,artistBurl=artistBurl)
 	
+@app.route("/genre/hip_hop/brockhampton/")
+def brockhampton():
+	artistPic=url_for('static',filename='bhPic.jpg')
+	return render_template('bh.html',artistPic=artistPic)
 
-@app.route("/hip_hop/brockhampton/iridescence/")
+@app.route("/genre/hip_hop/brockhampton/iridescence/")
+@app.route("/album/iridescence/")
 def iridescence():
 	album='iridescence'
 	artist='BROCKHAMPTON'
@@ -89,8 +94,13 @@ def iridescence():
 	genre='Hip Hop'
 	return render_template('album.html',album=album,artist=artist,genre=genre ,date=date,cover=cover,review=review,rating=rating,tracks=tracks,length=length,label=label)
 
+@app.route("/genre/house/daft_punk/")
+def daftPunk():
+	artistPic=url_for('static',filename='daftPunkPic.jpg')
+	return render_template('daftpunk.html',artistPic=artistPic)
 
-@app.route("/house/daft_punk/alive/")
+@app.route("/genre/house/daft_punk/alive/")
+@app.route("/album/alive/")
 def alive():
 	album='Alive (2007)'
 	artist='Daft Punk'
@@ -103,8 +113,14 @@ def alive():
 	review='Daft Punk deliver one of the most iconic and unforgettable live albums of all time with Alive(2007). No dance artist is more well known and influential than Daft Punk, they boast a massive library of hit songs that even the most casual fans can sing and bop around to, but Alive is much more than just a dance album. The French duo manage to seamlessly blend a massive 26 songs into one continuous mashup. The sound of the crowd mixed with pumping bass lines, steady drumbeats and unforgettable lyrics make this one of Daft Punk\'s most memorable listens that you can revisit to time and time again. Old classics that you thought you knew get a breath of fresh air in Alive and each song is mixed and used flawlessly. Simply put that if you like to dance you will like this album, the old tracks make Alive a classic but the way they\'re used throughout the double LP make it abrand new experience as well. Daft Punk\,s Alive is dance music at it\'s very best.'
 	genre='House, French House'
 	return render_template('album.html',album=album,genre=genre, artist=artist,date=date,cover=cover,review=review,rating=rating,tracks=tracks,length=length,label=label)
+
+@app.route("/genre/RnB/frank_ocean/")
+def frankocean():
+	artistPic=url_for('static',filename="frankPic.png")
+	return render_template('frankocean.html',artistPic=artistPic)
 	
-@app.route("/RnB/frank_ocean/channel_orange/")
+@app.route("/genre/RnB/frank_ocean/channel_orange/")
+@app.route("/album/channel_orange/")
 def orange():
 	album="Channel Orange"
 	artist="Frank Ocean"
@@ -118,7 +134,8 @@ def orange():
 	review="Odd Future member Frank Ocean\'s debut solo album, Channel Orange, is about love and life in Los Angeles. Frank Ocean has brought an unconventional musical style to this album, which describes a relationship which he had with a man and is full with emotional, tender and engrossing lyrics over almost sampleless beats across a wide range of styles and influences. The Album is beautiful, and by the end of the record, wich sits at just over an hour long, you'll want to listen to it again and again. The album is great in terms of music production and lyrical production and has excellent replay value. And despite some faults Frank Ocean really shines on Channel Orange, making him one to look out for in the future. Is this album perfect? No, every album has its ups and downs, but Channel Orange proves that Frank Ocean is one of the most exciting artists out in the world today. He is not afraid to talk about the stigmas that he may be subject to in his life - his bisexuality for example - and is taking R&B to heights which it has not reached in years. I'm excited to see what this artist brings to the table in the future, as he has shown here that he doesn't need to be a part of odd future to have a voice,  but for now Channel Orange is a suitable LP to keep your R&B needs at bay." 
 	return render_template('album.html',genre=genre,album=album,artist=artist,date=date,cover=cover,review=review,rating=rating,tracks=tracks,length=length,label=label)
 	
-@app.route("/house/jamie_xx/in_colour/")
+@app.route("/genre/house/jamie_xx/in_colour/")
+@app.route("/album/in_colour/")
 def in_colour():
 	album="In Colour"
 	artist="Jamie xx"
@@ -131,8 +148,14 @@ def in_colour():
 	genre="House, Electronica, Rave, Folktronica, UK Garage"
 	review="The xx producer and band member Jamie xx's debut solo LP, In Colour, may have taken almost a decade to produce, but you can see why when you give it a listen. In Colour is rich, complex and dazzlingly fluid, listening to it is a pleasure. Jaime brings the excellent sound production that we know and love from his work in the xx and puts it on full display, each song sounds different and unique in its own right, but you'll want to dance to all of them. This album isn't too over the top, the beats are simplistic but beautifully layered with outstanding build-ups that make the final product so enjoyable to listen to. Stand out songs include Loud Places which features Romy, Jaime\'s bandmate from the xx, and the opening track Gosh which features some of the strangest and surreal beats on the album. Overall this album is excellent, listening to each track back-to-back is as enjoyable everytime, In Colour shows what makes one of Jamie xx one of the best producers in dance right now."
 	return render_template('album.html',genre=genre,album=album,artist=artist,date=date,cover=cover,review=review,rating=rating,tracks=tracks,length=length,label=label)
+
+@app.route("/genre/hip_hop/kids_see_ghosts/")
+def kidsSeeGhosts():
+	artistPic=url_for('static',filename='ksgPic.jpg')
+	return render_template('ksg.html',artistPic=artistPic)
 	
-@app.route("/hip_hop/kids_see_ghosts/kids_see_ghosts/")
+@app.route("/genre/hip_hop/kids_see_ghosts/kids_see_ghosts/")
+@app.route("/album/kids_see_ghosts/")
 def KSG():
 	album="Kids See Ghosts"
 	artist="Kids See Ghosts"
@@ -146,7 +169,8 @@ def KSG():
 	review='Earlier this year Kanye West dropped his latest solo album \'ye\', and went on to recieve widespread acclaim. Now, Kanye has teamed up with rapper Kid Cudi to form the new hip hop duo \'Kids See Ghosts\'. With only 7 tracks and reaching a total runtime of less than 25 minutes Kids See Ghosts debut self-titled album makes a point to keep things to the point. Some may argue that there isn\'t enough content on this album, and I admit I was also left wanting more after listening to it, but the reason that this album stick out so much is because all 7 tracks are superb. Each song is perfect length and doesn\'t overstay its welcome. Kanye West and Cudi are on top form on this project which is always welcome. Once again Kanye\'s impressive producing skills show through and the album also includes some welcome features such as Ty Dolla Sign and Pusha T. There is a clear psychodelic influence on this album which gives the album a somewhat mellow vibe at times. Kids See Ghosts is definitely one of 2018\'s best hip hop albums, the flow of the album rapid and break-neck at times but each song stands out so much that you forgive the short run time of this excellent album. Hopefully Kanye West can continue his success with his upcoming album \'Yandi\', his third highly anticipated album of the year.'
 	return render_template('album.html',genre=genre,album=album,artist=artist,date=date,cover=cover,review=review,rating=rating,tracks=tracks,length=length,label=label)
 
-@app.route("/indie_rock/the_strokes/is_this_it/")
+@app.route("/genre/indie_rock/the_strokes/is_this_it/")
+@app.route("/album/is_this_it/")
 def is_this_it():
 	album="Is This It"
 	artist="The Strokes"
@@ -160,7 +184,8 @@ def is_this_it():
 	review="The Strokes now iconic debut album is a must have album to own for any Rock fan. Julian Casablancas brings a certain charms and swagger to each song, the basslines are catchy, the drums make you want to tap your feet, and yes the theme of sex, drugs and rock & roll may have been done in the past, but theres something about Is This It which makes it refreshing. This album also contains some essential songs for anybodies music library, Last Nite is a classic, you recognise it as soon as it comes on, and the lyrics are catchy, it's siimply impossible not to sing and dance to. This album just oozes charisma, if you like a single Strokes song then you'll love Is This It. And although the familair themes and formulaic structure of some of the songs make the album feel trite at times you can't deny that it isn't an excellent album thats gone on to inspire many of todays indie rock artists. If you haven't already given this classic a spin then you should definitely add it to your queue, you won't regret it."
 	return render_template('album.html',genre=genre,album=album,artist=artist,date=date,cover=cover,review=review,rating=rating,tracks=tracks,length=length,label=label)
 
-@app.route("/indie_rock/the_xx/xx/")
+@app.route("/genre/indie_rock/the_xx/xx/")
+@app.route("/album/xx/")
 def xx():
 	album='xx'
 	artist='The xx'
@@ -174,7 +199,8 @@ def xx():
 	review="South London band The xx's debut album xx features somber moments, fantastic production, moody vocals, twangy guitar strings, big basslines and heart-wrenching lyrics at times. The xx certainly have a sound that is their own and they know that, accross the album there is surprisingly little filler over the 11 tracks, every song feels perfectly realised and tells it's own story. A lot of the themes on this album revolve around struggling in love, and depicts the members talking about previous loves, letting go of ones you love, and still needing someone who no longer needs you. It's a message that's not new but one that everyone can get behind. The album starts off strong which is a fantastic opening track which perfectly sets the tone of the album, Intro doesn't even have any lyrics but its a song I find myself relistening to. Some of the dreamiest moments come from songs like \'Stars\' and \'Fantasy\' which gives us some of the strangest melodies on the project with quiet floaty beats over some fantastic guitar strings. xx is a listening experience from start to finish, it's an easy listening album, none of the music is offensive, it's just a relaxing, chilled out album thats easy to listen to and is one of 2009's best debut albums."
 	return render_template('album.html',genre=genre,album=album,artist=artist,date=date,cover=cover,review=review,rating=rating,tracks=tracks,length=length,label=label)
 
-@app.route("/RnB/childish_gambino/awaken_my_love/")
+@app.route("/genre/RnB/childish_gambino/awaken_my_love/")
+@app.route("/album/awaken_my_love/")
 def awaken_my_love():
 	album="Awaken, My Love!"
 	artist="Childish Gambino"
@@ -191,4 +217,3 @@ def awaken_my_love():
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
-
